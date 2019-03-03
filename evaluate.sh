fa5540695b835f7e719ea1525cf181fc0e725a11
littlesulley <494597918@qq.com>
2019-03-03 15:13:25 +0800

    update

--- evaluate.sh
+++ evaluate.sh
@@ -15,4 +15,7 @@ else
     FILE='pku'
 fi 
 
+echo "Running script..."
 perl scripts/score gold/${FILE}_training_words gold/${FILE}_test_gold checkpoint/${DATA_TYPE}/${DATA_TYPE}_pred > checkpoint/${DATA_TYPE}/score_${DATA_TYPE}
+
+echo $(tail -n 1 checkpoint/${DATA_TYPE}/score_${DATA_TYPE})
